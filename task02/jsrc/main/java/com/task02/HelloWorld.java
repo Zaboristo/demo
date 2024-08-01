674276e4b456bd9af994d9a18c6bf7cd424c8fd6
Aizenberg Boris <baizenbe@gmail.com>
2024-08-01 15:58:24 +0500

    Task_02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -2,53 +2,50 @@ package com.task02;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
-import com.syndicate.deployment.model.Architecture;
-import com.syndicate.deployment.model.ArtifactExtension;
-import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.syndicate.deployment.model.lambda.url.InvokeMode;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "hello_world",
-	roleName = "hello_world-role",
-	isPublishVersion = false,
-	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+        lambdaName = "hello_world",
+        roleName = "hello_world-role",
+        isPublishVersion = false,
+        logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @LambdaUrlConfig(
-		authType = AuthType.NONE
+        authType = AuthType.NONE
 )
 public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
-	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-		String path = event.getPath() == null ? "" : event.getPath();
-		Map<String, String> headers = new HashMap<>();
-		headers.put("Content-Type", "application/json");
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
+        String path = event.getPath() == null ? "" : event.getPath();
+        String resource = event.getResource() == null ? "" : event.getResource();
 
-		if (path.contains("\\hello")) {
-			APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-					.withStatusCode(200)
-					.withHeaders(headers)
-					.withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
-			return response;
-		} else {
-			// Handle other requests
-			String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", "/cmtr-b301d41c", "GET");
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        if (path.contains("\\hello")
+        || resource.contains("\\hello")) {
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                    .withStatusCode(200)
+                    .withHeaders(headers)
+                    .withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
+            return response;
+        } else {
+            // Handle other requests
+            String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", "/cmtr-b301d41c", "GET");
 
-			APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-					.withStatusCode(400)
-					.withHeaders(headers)
-					.withBody("{\"statusCode\": 400, \"message\": \"" + errorMessage + "\"}");
-			return response;
-		}
+            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                    .withStatusCode(400)
+                    .withHeaders(headers)
+                    .withBody("{\"statusCode\": 400, \"message\": \"" + errorMessage + "\"}");
+            return response;
+        }
 //		// Log the entire input event for debugging
 //		context.getLogger().log("Input event: " + event.toString());
 //
@@ -80,5 +77,5 @@ public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, A
 //					.withBody("{\"statusCode\": 400, \"message\": \"" + errorMessage + "\"}");
 //			return response;
 //		}
-	}
+    }
 }
