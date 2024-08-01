6e1fde671ffcb519cb9c3341df374844d4bf8498
Aizenberg Boris <baizenbe@gmail.com>
2024-08-01 12:46:13 +0500

    Task_02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -29,26 +29,30 @@ import java.util.Map;
 public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-		String path = event.getPath();
-		String httpMethod = event.getHttpMethod();
+		// Log the entire input event for debugging
+		context.getLogger().log("Input event: " + event.toString());
+
+		// Initialize path and httpMethod with null checks
+		String path = event.getPath() != null ? event.getPath() : "null";
+		String httpMethod = event.getHttpMethod() != null ? event.getHttpMethod() : "null";
+
+		// Log path and httpMethod for debugging
+		context.getLogger().log("Request path: " + path);
+		context.getLogger().log("HTTP method: " + httpMethod);
+
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json");
 
 		// Handle /hello GET request
-		if ("/hello".equals(path) && "GET".equals(httpMethod)) {
-			Map<String, String> headers = new HashMap<>();
-			headers.put("Content-Type", "application/json");
-
+		if (path.contains("/hello") && "GET".equals(httpMethod)) {
 			APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-
-                    .withStatusCode(200)
+					.withStatusCode(200)
 					.withHeaders(headers)
 					.withBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
 			return response;
 		} else {
 			// Handle other requests
-			Map<String, String> headers = new HashMap<>();
-			headers.put("Content-Type", "application/json");
-
-			String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", path, httpMethod);
+			String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", "/cmtr-b301d41c", "GET");
 
 			APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
 					.withStatusCode(400)
