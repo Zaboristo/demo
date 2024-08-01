e5a620034323e1d82218f013b854768878593afe
Aizenberg Boris <baizenbe@gmail.com>
2024-08-01 12:09:39 +0500

    Task_02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -3,9 +3,14 @@ package com.task02;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
+import com.syndicate.deployment.model.lambda.url.InvokeMode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,8 +29,14 @@ public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 	public Map<String, Object> handleRequest(Object request, Context context) {
 		System.out.println("Hello from lambda");
 		Map<String, Object> resultMap = new HashMap<String, Object>();
+		if (request.toString().contains("/hello")) {
 		resultMap.put("statusCode", 200);
 		resultMap.put("message", "Hello from Lambda");
+		} else {
+			resultMap.put("statusCode", 400);
+			resultMap.put("message", "Bad request syntax or unsupported method. Request path: {path}. HTTP method: {method}");
+		}
+
 		return resultMap;
 	}
 }
