bcaa22c82114e35a6b3e4bafb157f7f158b8e101
Aizenberg Boris <baizenbe@gmail.com>
2024-08-01 16:36:07 +0500

    Task_02

--- task02/jsrc/main/java/com/task02/HelloWorld.java
+++ task02/jsrc/main/java/com/task02/HelloWorld.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.RetentionSetting;
@@ -21,27 +23,24 @@ import java.util.Map;
 @LambdaUrlConfig(
         authType = AuthType.NONE
 )
-public class HelloWorld implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class HelloWorld implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
 
-    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent event, Context context) {
-        String path = event.getPath();
-        String resource = event.getResource();
-        String method = event.getHttpMethod();
+    public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent requestEvent, Context context) {
+        String path = requestEvent.getRequestContext().getHttp().getPath();
+        String method = requestEvent.getRequestContext().getHttp().getMethod();
 
         Map<String, String> headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
         if ("/hello".equals(path) && "GET".equalsIgnoreCase(method)) {
-            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+            APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
             response.setStatusCode(200);
             response.setBody("{\"statusCode\": 200, \"message\": \"Hello from Lambda\"}");
             return response;
         } else {
             String errorMessage = String.format("Bad request syntax or unsupported method. Request path: %s. HTTP method: %s", "/cmtr-b301d41c", "GET");
-
-            APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
-                    .withStatusCode(400)
-                    .withHeaders(headers)
-                    .withBody("{\"statusCode\": 400, \"message\": \"" + errorMessage + "\"}");
+            APIGatewayV2HTTPResponse response = new APIGatewayV2HTTPResponse();
+            response.setStatusCode(400);
+            response.setBody("{\"statusCode\": 400, \"message\": \"" + errorMessage + "\"}");
             return response;
         }
     }
