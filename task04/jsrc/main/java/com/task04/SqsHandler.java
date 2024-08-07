60406ef43c9d9ed02671dfc4721d08e00b56c231
Aizenberg Boris <baizenbe@gmail.com>
2024-08-07 12:29:48 +0500

    Task_04

--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -3,10 +3,12 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.syndicate.deployment.annotations.events.FunctionResponseType;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
-import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,10 +19,12 @@ import java.util.Map;
 	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
-@LambdaUrlConfig(
-		authType = AuthType.NONE
-)
-public class Sqshandler implements RequestHandler<SQSEvent, Void> {
+@SqsTriggerEventSource(targetQueue = "async_queue",
+batchSize = 123,
+functionResponseTypes = FunctionResponseType.REPORT_BATCH_ITEM_FAILURES)
+@DependsOn(name = "async_queue",
+resourceType = ResourceType.SQS_QUEUE)
+public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 
 	public Void handleRequest(SQSEvent event, Context context) {
 		event.getRecords().forEach(record -> {
