09ee8bdffc926a48d73f44b2b20a6a7fe2450d1d
Aizenberg Boris <baizenbe@gmail.com>
2024-08-07 13:00:34 +0500

    Task_04

--- task04/jsrc/main/java/com/task04/HelloWorld.java
+++ task04/jsrc/main/java/com/task04/HelloWorld.java
@@ -2,8 +2,14 @@ package com.task04;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.syndicate.deployment.annotations.events.FunctionResponseType;
+import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -14,6 +20,14 @@ import java.util.Map;
 	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@SqsTriggerEventSource(targetQueue = "async_queue",
+		batchSize = 123,
+		functionResponseTypes = FunctionResponseType.REPORT_BATCH_ITEM_FAILURES)
+@LambdaUrlConfig(
+		authType = AuthType.NONE
+)
+@DependsOn(name = "async_queue",
+		resourceType = ResourceType.SQS_QUEUE)
 public class HelloWorld implements RequestHandler<Object, Map<String, Object>> {
 
 	public Map<String, Object> handleRequest(Object request, Context context) {
--- task04/jsrc/main/java/com/task04/SqsHandler.java
+++ task04/jsrc/main/java/com/task04/SqsHandler.java
@@ -6,9 +6,11 @@ import com.amazonaws.services.lambda.runtime.events.SQSEvent;
 import com.syndicate.deployment.annotations.events.FunctionResponseType;
 import com.syndicate.deployment.annotations.events.SqsTriggerEventSource;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.resources.DependsOn;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.model.lambda.url.AuthType;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,6 +26,9 @@ batchSize = 123,
 functionResponseTypes = FunctionResponseType.REPORT_BATCH_ITEM_FAILURES)
 @DependsOn(name = "async_queue",
 resourceType = ResourceType.SQS_QUEUE)
+@LambdaUrlConfig(
+		authType = AuthType.NONE
+)
 public class SqsHandler implements RequestHandler<SQSEvent, Void> {
 	public Void handleRequest(SQSEvent event, Context context) {
 		event.getRecords().forEach(record -> {
