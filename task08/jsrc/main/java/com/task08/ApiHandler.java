ec04d96ab71d0e911b0647917a7ca7c1b15cbd07
Aizenberg Boris <baizenbe@gmail.com>
2024-08-10 15:21:47 +0500

    Task_08

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -2,17 +2,14 @@ package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.model.Architecture;
 import com.syndicate.deployment.model.ArtifactExtension;
 import com.syndicate.deployment.model.DeploymentRuntime;
-import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
-import com.meteo.OpenMeteoClient;
+import com.syndicate.deployment.model.RetentionSetting;
 
 @LambdaHandler(
     lambdaName = "api_handler",
@@ -33,7 +30,6 @@ public class ApiHandler implements RequestHandler<Object, String> {
 	@Override
 	public String  handleRequest(Object request, Context context) {
 		OpenMeteoClient client = new OpenMeteoClient();
-		APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
 
 		try {
 			String weatherData = client.getWeatherForecast();
