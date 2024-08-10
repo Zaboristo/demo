84a8834aa7e2b375012ca0b26b33596553d5b16c
Aizenberg Boris <baizenbe@gmail.com>
2024-08-10 18:09:45 +0500

    Task_08

--- task08/jsrc/main/java/com/task08/ApiHandler.java
+++ task08/jsrc/main/java/com/task08/ApiHandler.java
@@ -28,7 +28,7 @@ import java.io.IOException;
 		libraries = "lib/weather-sdk-1.0.jar",
 		runtime = DeploymentRuntime.JAVA11,
 		architectures = {Architecture.ARM64},
-		artifactExtension = ArtifactExtension.ZIP
+		artifactExtension = ArtifactExtension.JAR
 )
 public class ApiHandler implements RequestHandler<Object, String> {
 
