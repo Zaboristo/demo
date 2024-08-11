af30ca6f890e43a2560675c4f48af5a9df8a8719
Aizenberg Boris <baizenbe@gmail.com>
2024-08-11 15:16:35 +0500

    Task_10

--- task10/jsrc/main/java/com/task10/ApiHandler.java
+++ task10/jsrc/main/java/com/task10/ApiHandler.java
@@ -9,6 +9,7 @@ import com.syndicate.deployment.annotations.environment.EnvironmentVariables;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
 import com.syndicate.deployment.annotations.resources.DependsOn;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.ResourceType;
 import com.syndicate.deployment.model.RetentionSetting;
 import com.syndicate.deployment.model.lambda.url.AuthType;
@@ -27,9 +28,10 @@ import static com.syndicate.deployment.model.environment.ValueTransformer.USER_P
 @LambdaHandler(
     lambdaName = "api_handler",
 	roleName = "api_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	runtime = DeploymentRuntime.JAVA17,
+	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+
 )
 @DependsOn(resourceType = ResourceType.COGNITO_USER_POOL, name = "${pool_name}")
 @DependsOn(resourceType = ResourceType.DYNAMODB_TABLE, name = "Tables")
